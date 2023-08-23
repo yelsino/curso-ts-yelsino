ee94bc34f97c7345616be80fd78f041f6ae33de3
Luis Fernando <darkfam97@gmail.com>
2023-08-22 21:54:05 -0500

    prueba3

---
+++ dist/Carros.js
@@ -0,0 +1,10 @@
+"use strict";
+class Carros {
+    constructor() {
+        this.llantas = 4;
+        this.nomnre = 'CARRO1';
+    }
+    VenderCarro() {
+        return 'carro vendido';
+    }
+}
