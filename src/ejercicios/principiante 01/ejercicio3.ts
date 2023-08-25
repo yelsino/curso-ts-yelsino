872cf6188cd8587e40292f9509d0b2df8d244676
axl-777 <jhonwer54@gmail.com>
2023-08-25 22:28:45 +0200

    yesl

--- src/ejercicios/principiante 01/ejercicio3.ts
+++ src/ejercicios/principiante 01/ejercicio3.ts
@@ -1,2 +1,25 @@
 // 3. Obtén la lista completa de ventas.
 
+import { BDTienda } from "../BDTienda";
+
+BDTienda;
+
+// const scd : Carro[] = vehiculos.filter((vehiculo) => {
+//     if(vehiculo.marca === 'Fiat'){
+//         return vehiculo
+//     }
+
+// });
+
+const guardar = BDTienda.clientes.map(function (cliente) {
+  return (
+    `nombres:` +
+    `   ${cliente.nombre}` +
+    ` codigo: ` +
+    ` ${cliente.codigo}` +
+    ` N de documento:` +
+    `${cliente.documento}`
+  );
+});
+
+console.log(guardar);
--- src/ejercicios/principiante 01/ejercicio4.ts
+++ src/ejercicios/principiante 01/ejercicio4.ts
@@ -1,1 +1,29 @@
 // 4. Encuentra el cliente con el código "CL-002" y muestra su nombre.
+
+
+
+
+import { BDTienda } from "../BDTienda"
+
+
+
+function encontrarClienteXcodigo(codigo:string){
+
+
+    const clienteEncontrado = BDTienda.clientes.find((cliente)=>cliente.codigo === codigo);
+
+    if(!clienteEncontrado){
+        return 'CLIENTE NO ENCONTRADO'
+    }
+
+    
+    return console.log(clienteEncontrado.nombre);
+    
+
+    
+
+}
+
+encontrarClienteXcodigo('CL-002')
+
+
