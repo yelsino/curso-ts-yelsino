d2271b596159a0b084be8710ec39fefc28dedd7c
yelsiño <yelsino321@gmai.com>
2023-09-21 20:36:59 -0500

    trabajar con fechas

--- src/ejercicios/intermedio 02/ejercicio13.ts
+++ src/ejercicios/intermedio 02/ejercicio13.ts
@@ -1,17 +1,37 @@
 // 13. Encuentra el vehículo más caro.
 
-import { match } from "assert";
+// import { match } from "assert";
+// import { BDTienda } from "../BDTienda";
+
+
+// const numeros  = BDTienda.vehiculos.map((vehiculo)=> {
+
+//  return  vehiculo.precio
+
+
+// })
+
+
+// const vehiculoMasCaro  = Math.max(...numeros)
+// console.log(vehiculoMasCaro)
+
+
+
+// 13. Encuentra el vehículo más caro.
+
 import { BDTienda } from "../BDTienda";
 
+function carroCaro() {
+  return BDTienda.vehiculos.reduce(
+    (carroActual, carroSiguiente) => {
+      return carroActual.precio > carroSiguiente.precio
+        ? carroActual
+        : carroSiguiente;
+    }
+  );
+}
 
-const numeros  = BDTienda.vehiculos.map((vehiculo)=> {
+const carroMascaro = carroCaro();
 
- return  vehiculo.precio
-
-
-})
-
-// console.log(numeros)
-
-const vehiculoMasCaro  = Math.max(...numeros)
-console.log(vehiculoMasCaro)
+console.log("El carro más caro es:");
+console.log(carroMascaro);
--- src/ejercicios/intermedio 02/ejercicio16.ts
+++ src/ejercicios/intermedio 02/ejercicio16.ts
@@ -1,12 +1,58 @@
+// 16. Muestra el cliente que realizó la compra más reciente.
 import { BDTienda } from "../BDTienda";
 
-// 16. Muestra el cliente que realizó la compra más reciente.
+// COMO RESOLVER?
+/**
+ * obtener todos los clientes
+ * obtener todos las compras
+ * obtener la compra mas actual
+ * ya que no puedo comparar entre strings. 
+ * debo convertir a cada fecha de la compra en numero(milisegundos)
+ * ya que está convertida hacer la comparación entre milisegundas.
+ */
 
+interface CompraCliente {
+    codigoCompra: string;
+    codigoVehiculo: string;
+    codigoCliente: string;
+    nombreCliente: string;
+    fechaCompra: string;
+    montoCompra: number;
+    nombreVehiculo: string;
+}
 
-const fechasDeCompras = BDTienda.ventas.filter((valor) =>{
+const ventas = BDTienda.ventas;
+const vehiculos = BDTienda.vehiculos;
+const clientes = BDTienda.clientes;
 
-     return valor.fechaCompra 
-    
-})
+const recentPurchase = (): CompraCliente => {
+    const orderVentas = ventas.sort(
+        (a, b) =>
+            new Date(b.fechaCompra).getTime() -
+            new Date(a.fechaCompra).getTime()
+    );
 
-console.log(fechasDeCompras)
+    const ventaReciente = orderVentas[0];
+
+    const vehiculo = vehiculos.find(
+        (vehiculo) => ventaReciente.vehiculo === vehiculo.codigo
+    );
+    const cliente = clientes.find(
+        (cliente) => ventaReciente.cliente === cliente.codigo
+    );
+
+    const compraCliente = {
+        codigoCompra: ventaReciente.codigo,
+        codigoVehiculo: ventaReciente.codigo,
+        codigoCliente: ventaReciente.cliente,
+        nombreCliente: cliente?.nombre ?? "",
+        fechaCompra: ventaReciente.fechaCompra,
+        montoCompra: vehiculo?.precio ?? 0,
+        nombreVehiculo: vehiculo?.nombre ?? "",
+    };
+    console.log(compraCliente);
+
+    return compraCliente;
+};
+
+recentPurchase();
