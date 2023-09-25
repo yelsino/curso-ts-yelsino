6aea6447ead998811c1eab53adaa85e610c68475
yelsiño <yelsino321@gmai.com>
2023-09-24 22:03:31 -0500

    cambios

--- src/ejercicios/intermedio 02/ejercicio20.ts
+++ src/ejercicios/intermedio 02/ejercicio20.ts
@@ -1,2 +1,18 @@
 // 20. Muestra todos los clientes y la cantidad de compras que han realizado.
 
+import { BDTienda } from "../BDTienda";
+
+const clientes = BDTienda.clientes
+
+
+interface ClienteDetalle {
+    nombreCliente: string;
+    codigoCliente: string;
+    cantidadCompras: number;
+}
+
+function mostrarDatos():ClienteDetalle[]{
+
+
+    return []
+}
